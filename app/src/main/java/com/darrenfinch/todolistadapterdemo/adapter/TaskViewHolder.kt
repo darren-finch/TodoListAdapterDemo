dad685c02e4328a41a2ec9c83b2394ebf661b14d
Darren <finchdarren25@gmail.com>
2020-05-17 16:21:20 -0500

    Part 2 tutorial app finished. The card views are now expandable with animations.

--- app/src/main/java/com/darrenfinch/todolistadapterdemo/adapter/TaskViewHolder.kt
+++ app/src/main/java/com/darrenfinch/todolistadapterdemo/adapter/TaskViewHolder.kt
@@ -2,14 +2,36 @@ package com.darrenfinch.todolistadapterdemo.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.darrenfinch.todolistadapterdemo.AnimationUtils
+import com.darrenfinch.todolistadapterdemo.R
 import com.darrenfinch.todolistadapterdemo.Task
 import kotlinx.android.synthetic.main.task_item.view.*
 
 class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 {
+    private var isExpanded = false
     fun bind(task: Task)
     {
         itemView.taskName.text = task.taskName
         itemView.taskDescription.text = task.taskDescription
+        itemView.dropdownButton.setOnClickListener {
+            if (isExpanded) collapse() else expand()
+        }
+
+        //All tasks should be collapsed by default
+        collapse()
+    }
+
+    private fun expand()
+    {
+        AnimationUtils.expand(itemView.taskDescription)
+        itemView.dropdownButton.setImageResource(R.drawable.ic_keyboard_arrow_up)
+        isExpanded = true
+    }
+    private fun collapse()
+    {
+        AnimationUtils.collapse(itemView.taskDescription)
+        itemView.dropdownButton.setImageResource(R.drawable.ic_keyboard_arrow_down)
+        isExpanded = false
     }
 }
